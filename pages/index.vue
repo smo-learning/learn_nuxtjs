b5e8d61183c0bc129a1cc147265b5390fc214727
Allquantor <nilssmol@gmail.com>
2019-02-28 11:59:54 +0100

    Add Phrase

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,18 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        learn_nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My primo Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <div>{{ text }}</div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import axios from 'axios'
 export default {
-  components: {
-    Logo
+  asyncData(context) {
+    return axios
+      .get('https://corporatebs-generator.sameerkumar.website/')
+      .then(res => {
+        return { text: res.data.phrase }
+      })
   }
 }
 </script>
@@ -42,27 +25,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
